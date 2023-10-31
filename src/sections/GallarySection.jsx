310719aca3ddf631fa93de4137560de0611afaf4
MD. Abdul Kader <56482597+abdulkadershohan@users.noreply.github.com>
2023-10-31 22:13:22 +0600

    image src update

--- src/sections/GallarySection.jsx
+++ src/sections/GallarySection.jsx
@@ -3,47 +3,47 @@ import ImageIcon from '../components/ImageIcon'
 const DATA = [
     {
         id: 1,
-        image: require('../images/image-1.webp'),
+        image: require('../assets/images/image-1.webp'),
     },
     {
         id: 2,
-        image: require('../images/image-2.webp'),
+        image: require('../assets/images/image-2.webp'),
     },
     {
         id: 3,
-        image: require('../images/image-3.webp'),
+        image: require('../assets/images/image-3.webp'),
     },
     {
         id: 4,
-        image: require('../images/image-4.webp'),
+        image: require('../assets/images/image-4.webp'),
     },
     {
         id: 5,
-        image: require('../images/image-5.webp'),
+        image: require('../assets/images/image-5.webp'),
     },
     {
         id: 6,
-        image: require('../images/image-6.webp'),
+        image: require('../assets/images/image-6.webp'),
     },
     {
         id: 7,
-        image: require('../images/image-7.webp'),
+        image: require('../assets/images/image-7.webp'),
     },
     {
         id: 8,
-        image: require('../images/image-8.webp'),
+        image: require('../assets/images/image-8.webp'),
     },
     {
         id: 9,
-        image: require('../images/image-9.webp'),
+        image: require('../assets/images/image-9.webp'),
     },
     {
         id: 10,
-        image: require('../images/image-10.jpeg'),
+        image: require('../assets/images/image-10.jpeg'),
     },
     {
         id: 11,
-        image: require('../images/image-11.jpeg'),
+        image: require('../assets/images/image-11.jpeg'),
     }
 ]
 
@@ -58,7 +58,7 @@ export default function GallarySection() {
                 {/* <div
                     className='grid-cols-4 w-64 h-64 rounded-lg overflow-hidden border-2 border-gray-200'
                 >
-                    <img src={require('../images/image-1.webp')} alt="sds" />
+                    <img src={require('../assets/images/image-1.webp')} alt="sds" />
                 </div> */}
                 {
                     DATA.map((item) => (
